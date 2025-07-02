8c546bf573e1611ab3c94f18d763269571ab9e00
Gabriele Balciunaite <gabrielebal92@gmail.com>
2025-07-02 14:27:39 +0200

    Add actual code to database_repository.dart

--- database_repository.dart
+++ database_repository.dart
@@ -1,17 +1,19 @@
+//aufgabe 2
 abstract class DatabaseRepository {
   void sendTransaction(Transaction transaction);
   List<Transaction> getTransactions();
 
   void updateSettings(AccountSetting setting);
-  void deleteTransaction(String transactionId);
+  void hideTransaction(String transactionId);
 }
 
 class Transaction {
   final String id;
   final double amount;
   final String description;
+  bool hidden;
 
-  Transaction(this.id, this.amount, this.description);
+  Transaction(this.id, this.amount, this.description, this.hidden);
 }
 
 class AccountSetting {
@@ -19,4 +21,35 @@ class AccountSetting {
   final bool childAccess;
 
   AccountSetting(this.limit, this.childAccess);
+}
+
+//aufgabe 3
+//method from the abstarct class
+class MockDatabaseRepository implements DatabaseRepository {
+  List<Transaction> transactions = [];
+  AccountSetting? settings;
+
+  @override
+  void sendTransaction(Transaction transaction) {
+    transactions.add(transaction);
+  }
+
+  @override
+  List<Transaction> getTransactions() {
+    return transactions;
+  }
+
+  @override
+  void updateSettings(AccountSetting setting) {
+    settings = setting;
+  }
+
+  //hide
+  void hideTransaction(String transactionId) {
+    for (var t in transactions) {
+      if (t.id == transactionId) {
+        t.hidden = true;
+      }
+    }
+  }
 }
